f09ad2d0844447745ffbd2d0ca376a65f5f22732
Motao Cao <motaocao@gmail.com>
2020-12-25 04:27:05 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -10,20 +10,6 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-data_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds = TabularDatasetFactory.from_delimited_files(data_path, separator = ',')
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-training_x, training_y, testing_x, testing_y = train_test_split(x, y, test_size = 0.3)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -50,7 +36,20 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     return x_df, y_df
-    
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+data_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(data_path, separator = ',')
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
+
+run = Run.get_context()    
 
 def main():
     # Add arguments to script
@@ -70,7 +69,7 @@ def main():
     run.log("Accuracy", np.float(accuracy))
 
     os.makedirs('./outputs', exist_ok = True)
-    joblib.dump(value = model, './output/model.joblib')
+    joblib.dump(value = model, filename='./outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
